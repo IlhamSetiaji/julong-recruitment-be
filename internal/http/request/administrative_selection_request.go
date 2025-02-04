615f484a64862f847671c7366304c44ef12e4a64
thethreestrikes <ilham.ahmadz18@student.uns.ac.id>
2025-02-04 10:40:09 +0700

    fix: update DocumentDate validation to require specific datetime format

--- internal/http/request/administrative_selection_request.go
+++ internal/http/request/administrative_selection_request.go
@@ -4,7 +4,7 @@ type CreateAdministrativeSelectionRequest struct {
 	JobPostingID   string `json:"job_posting_id" validate:"required,uuid"`
 	ProjectPicID   string `json:"project_pic_id" validate:"required,uuid"`
 	Status         string `json:"status" validate:"required,administrative_selection_status_validation"`
-	DocumentDate   string `json:"document_date" validate:"required,date"`
+	DocumentDate   string `json:"document_date" validate:"required,datetime=2006-01-02"`
 	DocumentNumber string `json:"document_number" validate:"required"`
 }
 
@@ -13,6 +13,6 @@ type UpdateAdministrativeSelectionRequest struct {
 	JobPostingID   string `json:"job_posting_id" validate:"required,uuid"`
 	ProjectPicID   string `json:"project_pic_id" validate:"required,uuid"`
 	Status         string `json:"status" validate:"required,administrative_selection_status_validation"`
-	DocumentDate   string `json:"document_date" validate:"required,date"`
+	DocumentDate   string `json:"document_date" validate:"required,datetime=2006-01-02"`
 	DocumentNumber string `json:"document_number" validate:"required"`
 }

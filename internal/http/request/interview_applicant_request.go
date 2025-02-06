a8d12e84682d537e9f077b2441af48f6e6d114c4
thethreestrikes <ilham.ahmadz18@student.uns.ac.id>
2025-02-06 12:06:02 +0700

    feat: add CreateOrUpdateInterviewApplicantsRequest and UpdateStatusInterviewApplicantRequest structs for managing interview applicants

---
+++ internal/http/request/interview_applicant_request.go
@@ -0,0 +1,19 @@
+package request
+
+type CreateOrUpdateInterviewApplicantsRequest struct {
+	InterviewID         string `json:"interview_id" validate:"required,uuid"`
+	InterviewApplicants []struct {
+		ID            string `json:"id" validate:"omitempty,uuid"`
+		ApplicantID   string `json:"applicant_id" validate:"required,uuid"`
+		UserProfileID string `json:"user_profile_id" validate:"required,uuid"`
+		StartTime     string `json:"start_time" validate:"required,datetime=2006-01-02 15:04:05"`
+		EndTime       string `json:"end_time" validate:"required,datetime=2006-01-02 15:04:05"`
+		FinalResult   string `json:"final_result" validate:"required,interview_applicant_status_validation"`
+	} `json:"interview_applicants" validate:"required,dive"`
+	DeletedInterviewApplicantIDs []string `json:"deleted_interview_applicant_ids" validate:"omitempty,dive,uuid"`
+}
+
+type UpdateStatusInterviewApplicantRequest struct {
+	ID     string `json:"id" validate:"required,uuid"`
+	Status string `json:"status" validate:"required,assessment_status_validation"`
+}
---
+++ internal/http/usecase/interview_applicant_usecase.go
@@ -0,0 +1,317 @@
+package usecase
+
+import (
+	"errors"
+	"time"
+
+	"github.com/IlhamSetiaji/julong-recruitment-be/internal/dto"
+	"github.com/IlhamSetiaji/julong-recruitment-be/internal/entity"
+	"github.com/IlhamSetiaji/julong-recruitment-be/internal/http/request"
+	"github.com/IlhamSetiaji/julong-recruitment-be/internal/http/response"
+	"github.com/IlhamSetiaji/julong-recruitment-be/internal/repository"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type IInterviewApplicantUseCase interface {
+	CreateOrUpdateInterviewApplicants(req *request.CreateOrUpdateInterviewApplicantsRequest) (*response.InterviewResponse, error)
+	UpdateStatusInterviewApplicant(req *request.UpdateStatusInterviewApplicantRequest) error
+	FindByUserProfileIDAndInterviewID(userProfileID, interviewID string) (*response.InterviewApplicantResponse, error)
+	FindAllByInterviewIDPaginated(interviewID string, page, pageSize int, search string, sort map[string]interface{}, filter map[string]interface{}) ([]response.InterviewApplicantResponse, int64, error)
+}
+
+type InterviewApplicantUseCase struct {
+	Log                   *logrus.Logger
+	Repository            repository.IInterviewApplicantRepository
+	DTO                   dto.IInterviewApplicantDTO
+	InterviewRepository   repository.IInterviewRepository
+	InterviewDTO          dto.IInterviewDTO
+	UserProfileRepository repository.IUserProfileRepository
+	Viper                 *viper.Viper
+	ApplicantRepository   repository.IApplicantRepository
+}
+
+func NewInterviewApplicantUseCase(
+	log *logrus.Logger,
+	repository repository.IInterviewApplicantRepository,
+	iaDto dto.IInterviewApplicantDTO,
+	interviewRepository repository.IInterviewRepository,
+	interviewDTO dto.IInterviewDTO,
+	userProfileRepository repository.IUserProfileRepository,
+	viper *viper.Viper,
+	applicantRepo repository.IApplicantRepository,
+) IInterviewApplicantUseCase {
+	return &InterviewApplicantUseCase{
+		Log:                   log,
+		Repository:            repository,
+		DTO:                   iaDto,
+		InterviewRepository:   interviewRepository,
+		InterviewDTO:          interviewDTO,
+		UserProfileRepository: userProfileRepository,
+		Viper:                 viper,
+		ApplicantRepository:   applicantRepo,
+	}
+}
+
+func InterviewApplicantUseCaseFactory(
+	log *logrus.Logger,
+	repository repository.IInterviewApplicantRepository,
+	iaDto dto.IInterviewApplicantDTO,
+	interviewRepository repository.IInterviewRepository,
+	interviewDTO dto.IInterviewDTO,
+	userProfileRepository repository.IUserProfileRepository,
+	viper *viper.Viper,
+	applicantRepo repository.IApplicantRepository,
+) IInterviewApplicantUseCase {
+	return NewInterviewApplicantUseCase(
+		log,
+		repository,
+		iaDto,
+		interviewRepository,
+		interviewDTO,
+		userProfileRepository,
+		viper,
+		applicantRepo,
+	)
+}
+
+func (uc *InterviewApplicantUseCase) CreateOrUpdateInterviewApplicants(req *request.CreateOrUpdateInterviewApplicantsRequest) (*response.InterviewResponse, error) {
+	// check if interview exist
+	parsedInterviewID, err := uuid.Parse(req.InterviewID)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error parsing interview id: %v", err)
+		return nil, err
+	}
+	interview, err := uc.InterviewRepository.FindByID(parsedInterviewID)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error finding interview by id: %v", err)
+		return nil, err
+	}
+	if interview == nil {
+		return nil, errors.New("interview not found")
+	}
+
+	// create or update interview applicants
+	for _, ia := range req.InterviewApplicants {
+		parsedUserProfileID, err := uuid.Parse(ia.UserProfileID)
+		if err != nil {
+			uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error parsing user profile id: %v", err)
+			return nil, err
+		}
+		up, err := uc.UserProfileRepository.FindByID(parsedUserProfileID)
+		if err != nil {
+			uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error finding user profile by id: %v", err)
+			return nil, err
+		}
+		if up == nil {
+			return nil, errors.New("user profile not found")
+		}
+
+		parsedApplicantID, err := uuid.Parse(ia.ApplicantID)
+		if err != nil {
+			uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error parsing applicant id: %v", err)
+			return nil, err
+		}
+		applicant, err := uc.ApplicantRepository.FindByKeys(map[string]interface{}{
+			"id": parsedApplicantID,
+		})
+		if err != nil {
+			uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error finding applicant by id: %v", err)
+			return nil, err
+		}
+		if applicant == nil {
+			return nil, errors.New("applicant not found")
+		}
+
+		parsedStartTime, err := time.Parse("2006-01-02 15:04:05", ia.StartTime)
+		if err != nil {
+			uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error parsing start time: %v", err)
+			return nil, err
+		}
+
+		parsedEndTime, err := time.Parse("2006-01-02 15:04:05", ia.EndTime)
+		if err != nil {
+			uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error parsing end time: %v", err)
+			return nil, err
+		}
+
+		if ia.ID != "" && ia.ID != uuid.Nil.String() {
+			parsedIaID, err := uuid.Parse(ia.ID)
+			if err != nil {
+				uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error parsing interview applicant id: %v", err)
+				return nil, err
+			}
+			exist, err := uc.Repository.FindByID(parsedIaID)
+			if err != nil {
+				uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error finding interview applicant by id: %v", err)
+				return nil, err
+			}
+			if exist == nil {
+				_, err := uc.Repository.CreateInterviewApplicant(&entity.InterviewApplicant{
+					InterviewID:   parsedInterviewID,
+					ApplicantID:   parsedApplicantID,
+					UserProfileID: parsedUserProfileID,
+					StartTime:     parsedStartTime,
+					EndTime:       parsedEndTime,
+					FinalResult:   entity.FinalResultStatus(ia.FinalResult),
+				})
+				if err != nil {
+					uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error creating interview applicant: %v", err)
+					return nil, err
+				}
+			} else {
+				_, err := uc.Repository.UpdateInterviewApplicant(&entity.InterviewApplicant{
+					ID:            parsedIaID,
+					InterviewID:   parsedInterviewID,
+					ApplicantID:   parsedApplicantID,
+					UserProfileID: parsedUserProfileID,
+					StartTime:     parsedStartTime,
+					EndTime:       parsedEndTime,
+					FinalResult:   entity.FinalResultStatus(ia.FinalResult),
+				})
+				if err != nil {
+					uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error updating interview applicant: %v", err)
+					return nil, err
+				}
+			}
+		} else {
+			_, err := uc.Repository.CreateInterviewApplicant(&entity.InterviewApplicant{
+				InterviewID:   parsedInterviewID,
+				ApplicantID:   parsedApplicantID,
+				UserProfileID: parsedUserProfileID,
+				StartTime:     parsedStartTime,
+				EndTime:       parsedEndTime,
+				FinalResult:   entity.FinalResultStatus(ia.FinalResult),
+			})
+			if err != nil {
+				uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error creating interview applicant: %v", err)
+				return nil, err
+			}
+		}
+	}
+
+	// delete interview applicants
+	if len(req.DeletedInterviewApplicantIDs) > 0 {
+		for _, id := range req.DeletedInterviewApplicantIDs {
+			parsedID, err := uuid.Parse(id)
+			if err != nil {
+				uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error parsing interview applicant id: %v", err)
+				return nil, err
+			}
+			err = uc.Repository.DeleteInterviewApplicant(parsedID)
+			if err != nil {
+				uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error deleting interview applicant: %v", err)
+				return nil, err
+			}
+		}
+	}
+
+	// get interview
+	interviewRes, err := uc.InterviewRepository.FindByID(parsedInterviewID)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error finding interview by id: %v", err)
+		return nil, err
+	}
+	if interviewRes == nil {
+		return nil, errors.New("interview not found")
+	}
+
+	// convert interview to response
+	resp, err := uc.InterviewDTO.ConvertEntityToResponse(interviewRes)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.CreateOrUpdateInterviewApplicants] error converting interview to response: %v", err)
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (uc *InterviewApplicantUseCase) UpdateStatusInterviewApplicant(req *request.UpdateStatusInterviewApplicantRequest) error {
+	parsedID, err := uuid.Parse(req.ID)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.UpdateStatusInterviewApplicant] error parsing interview applicant id: %v", err.Error())
+		return err
+	}
+
+	ia, err := uc.Repository.FindByID(parsedID)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.UpdateStatusInterviewApplicant] error finding interview applicant by id: %v", err.Error())
+		return err
+	}
+
+	if ia == nil {
+		return errors.New("interview applicant not found")
+	}
+
+	_, err = uc.Repository.UpdateInterviewApplicant(&entity.InterviewApplicant{
+		ID:               parsedID,
+		AssessmentStatus: entity.AssessmentStatus(req.Status),
+	})
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.UpdateStatusInterviewApplicant] error updating interview applicant status: %v", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (uc *InterviewApplicantUseCase) FindByUserProfileIDAndInterviewID(userProfileID, interviewID string) (*response.InterviewApplicantResponse, error) {
+	parsedUserProfileID, err := uuid.Parse(userProfileID)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.FindByUserProfileIDAndInterviewID] error parsing user profile id: %v", err)
+		return nil, err
+	}
+
+	parsedInterviewID, err := uuid.Parse(interviewID)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.FindByUserProfileIDAndInterviewID] error parsing interview id: %v", err)
+		return nil, err
+	}
+
+	exist, err := uc.Repository.FindByKeys(map[string]interface{}{
+		"user_profile_id": parsedUserProfileID,
+		"interview_id":    parsedInterviewID,
+	})
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.FindByUserProfileIDAndInterviewID] error finding interview applicant by user profile id and interview id: %v", err)
+		return nil, err
+	}
+	if exist == nil {
+		return nil, nil
+	}
+
+	resp, err := uc.DTO.ConvertEntityToResponse(exist)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.FindByUserProfileIDAndInterviewID] error converting interview applicant to response: %v", err)
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (uc *InterviewApplicantUseCase) FindAllByInterviewIDPaginated(interviewID string, page, pageSize int, search string, sort map[string]interface{}, filter map[string]interface{}) ([]response.InterviewApplicantResponse, int64, error) {
+	parsedInterviewID, err := uuid.Parse(interviewID)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.FindAllByInterviewIDPaginated] error parsing interview id: %v", err)
+		return nil, 0, err
+	}
+
+	exist, count, err := uc.Repository.FindAllByInterviewIDPaginated(parsedInterviewID, page, pageSize, search, sort, filter)
+	if err != nil {
+		uc.Log.Errorf("[InterviewApplicantUseCase.FindAllByInterviewIDPaginated] error finding interview applicants by interview id: %v", err)
+		return nil, 0, err
+	}
+
+	var res []response.InterviewApplicantResponse
+	for _, ia := range exist {
+		resp, err := uc.DTO.ConvertEntityToResponse(&ia)
+		if err != nil {
+			uc.Log.Errorf("[InterviewApplicantUseCase.FindAllByInterviewIDPaginated] error converting interview applicant to response: %v", err)
+			return nil, 0, err
+		}
+		res = append(res, *resp)
+	}
+
+	return res, count, nil
+}

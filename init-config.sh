73f2a9444e8430cf112f22c9fc520dabc459bccf
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2025-03-14 10:00:04 +0700

    feat: copy initial files to the volume if they don't exist in init-config.sh

--- init-config.sh
+++ init-config.sh
@@ -2,4 +2,9 @@
 
 envsubst < /app/config.template.json > /app/config.json
 
+# Copy initial files to the volume if they don't exist
+if [ -d "/app/storage" ]; then
+  cp -n -r /app/storage/* /storage/
+fi
+
 exec ./main

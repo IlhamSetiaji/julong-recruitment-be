4f7cb574c54940255b9d99fc3bc36faa7039d847
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2025-03-14 10:08:38 +0700

    refactor: remove redundant file copy logic in init-config.sh

--- init-config.sh
+++ init-config.sh
@@ -2,9 +2,4 @@
 
 envsubst < /app/config.template.json > /app/config.json
 
-# Copy initial files to the volume if they don't exist
-if [ -d "/app/storage" ]; then
-  cp -n -r /app/storage/* /storage/
-fi
-
 exec ./main
